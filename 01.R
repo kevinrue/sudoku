6900cd35f70fa24df0d2db393ccdc5b388e1ae3d
Kevin Rue <kevinrue@users.noreply.github.com>
2023-06-20 22:32:13 +0100

    get cell choices

--- 01.R
+++ 01.R
@@ -8,8 +8,8 @@ initiate_empty_grid <- function() {
     arrange(row, column)
 }
 
-add_value_xy <- function(grid, row, column, value) {
-  idx <- which(grid$row == row & grid$column == column)
+add_value_xy <- function(grid, .row, .column, value) {
+  idx <- which(grid$row == .row & grid$column == .column)
   grid[idx, "value"] <- value
   grid
 }
@@ -48,3 +48,25 @@ sudoku_grid <- add_value_xy(sudoku_grid, 9, 6, 6)
 sudoku_grid <- add_value_xy(sudoku_grid, 9, 9, 3)
 plot_grid(sudoku_grid)
 
+get_tile_values <- function(grid, .row, .column) {
+  tile_row <- .row %/% m + 1
+  tile_column <- .column %/% m + 1
+  get_rows <- 1:3 + (tile_row-1)*3
+  get_columns <- 1:3 + (tile_row-1)*3
+}
+
+get_cell_choices <- function(grid, .row, .column) {
+  choices <- 1:9
+  row_values_used <- grid %>% 
+    filter(row == .row & !is.na(value)) %>% 
+    pull(value)
+  message("row values used:", row_values_used)
+  choices <- setdiff(choices, row_values_used)
+  column_values_used <- grid %>% 
+    filter(column == .column & !is.na(value)) %>% 
+    pull(value)
+  message("column values used:", column_values_used)
+  choices <- setdiff(choices, column_values_used)
+  choices
+}
+get_cell_choices(sudoku_grid, 1, 3)
