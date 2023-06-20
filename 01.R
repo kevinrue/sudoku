9ed071aeba409cd855138012c1d024a861736d27
Kevin Rue <kevinrue@users.noreply.github.com>
2023-06-20 22:39:09 +0100

    fix getting tile values

--- 01.R
+++ 01.R
@@ -49,24 +49,44 @@ sudoku_grid <- add_value_xy(sudoku_grid, 9, 9, 3)
 plot_grid(sudoku_grid)
 
 get_tile_values <- function(grid, .row, .column) {
-  tile_row <- .row %/% m + 1
-  tile_column <- .column %/% m + 1
+  tile_row <- (.row-1) %/% 3 + 1
+  tile_column <- (.column-1) %/% 3 + 1
   get_rows <- 1:3 + (tile_row-1)*3
-  get_columns <- 1:3 + (tile_row-1)*3
+  get_columns <- 1:3 + (tile_column-1)*3
+  grid %>% 
+    filter(row %in% get_rows & column %in% get_columns & !is.na(value)) %>% 
+    pull(value)
 }
 
 get_cell_choices <- function(grid, .row, .column) {
   choices <- 1:9
+  
   row_values_used <- grid %>% 
     filter(row == .row & !is.na(value)) %>% 
     pull(value)
-  message("row values used:", row_values_used)
   choices <- setdiff(choices, row_values_used)
+  
   column_values_used <- grid %>% 
     filter(column == .column & !is.na(value)) %>% 
     pull(value)
-  message("column values used:", column_values_used)
   choices <- setdiff(choices, column_values_used)
+  
+  tile_values_used <- get_tile_values(grid, .row, .column)
+  choices <- setdiff(choices, tile_values_used)
+  
   choices
 }
 get_cell_choices(sudoku_grid, 1, 3)
+get_cell_choices(sudoku_grid, 2, 4)
+get_cell_choices(sudoku_grid, 3, 8)
+get_cell_choices(sudoku_grid, 6, 8)
+get_cell_choices(sudoku_grid, 8, 7)
+
+for (row in 1:9) {
+  for (column in 1:9) {
+    choices <- get_cell_choices(sudoku_grid, row, column)
+    if (length(choices) == 1) {
+      message("row: ", row, ", column: ", column, ", value: ", choices)
+    }
+  }
+}
