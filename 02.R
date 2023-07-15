f2c137688135a1394c50b71ad6504daf3bca293c
Kevin Rue <kevinrue@users.noreply.github.com>
2023-07-15 22:00:35 +0100

    solve the problem only_cell_in_tile

--- 02.R
+++ 02.R
@@ -43,7 +43,20 @@ plot_grid <- function(.grid) {
     theme_void() +
     scale_color_manual(values = c("TRUE" = "black", "FALSE" = "cornflowerblue"))
 }
-plot_grid(sudoku_grid)
+# plot_grid(sudoku_grid)
+
+plot_choices_value <- function(.choices, .value) {
+  tile_centers <- expand.grid(row = 2 + 0:2*3, column = 2 + 0:2*3)
+  .choices %>% 
+    filter(value == .value) %>% 
+  ggplot(aes(column, 10-row)) +
+    geom_tile(fill = "white", color = "black", width = 1, height = 1) +
+    geom_tile(aes(10-row, column), tile_centers, fill = NA, color = "black", width = 3, height = 3, linewidth = 2) +
+    geom_text(aes(label = value)) +
+    theme_void() +
+    scale_color_manual(values = c("TRUE" = "black", "FALSE" = "cornflowerblue"))
+}
+plot_choices_value(sudoku_choices, 1)
 
 sudoku_grid <- initiate_empty_grid()
 sudoku_grid <- add_value_xy(sudoku_grid, 1, 1, 2, given=TRUE)
@@ -90,7 +103,14 @@ get_tile_values <- function(.grid, .row, .column) {
     pull(value)
 }
 
-get_cell_choices <- function(.grid, .row, .column) {
+compute_cell_choices <- function(.grid, .row, .column) {
+  grid_value <- .grid %>% 
+    filter(row == .row & column == .column) %>% 
+    pull(value)
+  if (!is.na(grid_value)) {
+    return(grid_value)
+  }
+  
   choices <- 1:9
   
   row_values_used <- .grid %>% 
@@ -108,99 +128,143 @@ get_cell_choices <- function(.grid, .row, .column) {
   
   choices
 }
-get_cell_choices(sudoku_grid, 1, 3)
-get_cell_choices(sudoku_grid, 2, 4)
-get_cell_choices(sudoku_grid, 3, 8)
-get_cell_choices(sudoku_grid, 6, 8)
-get_cell_choices(sudoku_grid, 8, 7)
+compute_cell_choices(sudoku_grid, 1, 3)
+compute_cell_choices(sudoku_grid, 2, 4)
+compute_cell_choices(sudoku_grid, 3, 8)
+compute_cell_choices(sudoku_grid, 6, 8)
+compute_cell_choices(sudoku_grid, 8, 7)
+compute_cell_choices(sudoku_grid, 1, 2)
 
-only_cell_for_value <- function(.grid, .value, .row, .column) {
+get_cell_choices <- function(.grid, .row, .column) {
+  .grid %>% 
+    filter(row == .row & column == .column) %>% 
+    pull(value)
+}
+get_cell_choices(sudoku_choices, 8, 7)
+get_cell_choices(sudoku_choices, 1, 2)
+
+only_cell_in_tile <- function(.choices, .value, .row, .column) {
   row_tile <- get_tile_index(.row)
   column_tile <- get_tile_index(.column)
   get_rows <- get_tile_indices(row_tile)
   get_columns <- get_tile_indices(column_tile)
-  get_cells <- expand.grid(row=get_rows, column=get_columns)
-  
-  only_row <- .grid %>%
-    filter(row %in% get_rows & !is.na(value)) %>% 
-    group_by(row) %>% 
-    summarise(test = any(value == .value | (column == .column & !is.na(value)), na.rm = TRUE)) %>% 
-    pull()
-  only_column <- .grid %>%
-    filter(column %in% get_columns) %>% 
-    group_by(column) %>% 
-    summarise(test = any(value == .value | (row == .row & !is.na(value)), na.rm = TRUE)) %>% 
-    pull()
-  print(c(only_row, only_column))
-  all(only_row, only_column)
+  cells_in_tile <- .choices %>% 
+    filter(value == .value) %>% 
+    filter(row %in% get_rows & column %in% get_columns)
+  identical(nrow(cells_in_tile), 1L) &&
+    cells_in_tile$row == .row &&
+    cells_in_tile$column == .column
 }
-only_cell_for_value(sudoku_grid, 1, 3, 8)
+only_cell_in_tile(sudoku_choices, 1, 3, 8)
 
 prompt_next <- function() {
-  x <- readline(prompt="Go next?")
+  x <- readline(prompt="Go next? ")
   grepl("y", x)
 }
 
 test_choices_xy <- function(.grid, .choices, .row, .column) {
   # what are the choices for the current cell?
-  cell_choices <- .choices %>% 
-    filter(row == .row & column == .column) %>% 
-    pull(value)
+  cell_choices <- get_cell_choices(.choices, .row, .column)
+  message("== cell_choices ==")
+  message("row: ", .row, ", column: ", .column, ", choice: ", cell_choices)
   # what are the choices for every other cell in the tile?
   tile_row_index <- get_tile_index(.row)
   tile_column_index <- get_tile_index(.column)
   tile_row_indices <- get_tile_indices(tile_row_index)
   tile_column_indices <- get_tile_indices(tile_column_index)
-  tile_cells <- as_tibble(expand.grid(row = tile_row_indices, column = tile_column_indices)) %>% 
-    bind_rows(as_tibble(row = .row, column == setdiff(1:9, .column))) %>% 
-    bind_rows(as_tibble(row = setdiff(1:9, .row), column == .column)) %>% 
-    arrange(row, column)
-  other_cell_choices <- tile_cells %>% 
+  competing_cells <- as_tibble(expand.grid(row = tile_row_indices, column = tile_column_indices)) %>% 
+    bind_rows(tibble(row = .row, column = setdiff(1:9, .column))) %>% 
+    bind_rows(tibble(row = setdiff(1:9, .row), column = .column)) %>% 
     filter(!(row == .row & column ==.column)) %>% 
+    distinct(row, column) %>% 
+    arrange(row, column)
+  # print(c(.row, .column))
+  # print(competing_cells, n = Inf)
+  other_cell_choices <- competing_cells %>% 
     inner_join(.choices, c("row", "column")) %>% 
-    pull(value)
+    pull(value) %>% 
+    unique()
+  print(other_cell_choices)
   cell_choices[!cell_choices %in% other_cell_choices]
 }
 test_choices_xy(sudoku_grid, sudoku_choices, 3, 8)
 
+update_choices_all <- function(sudoku_choices, sudoku_grid) {
+  for (.row in 1:9) {
+    for (.column in 1:9) {
+      sudoku_grid_value <- sudoku_grid %>% 
+        filter(row == .row & column == .column) %>% 
+        pull(value)
+      if (!is.na(sudoku_grid_value)) {
+        sudoku_choices <- update_choices_xy(sudoku_choices, .row, .column, sudoku_grid_value)
+      } else {
+        choices <- compute_cell_choices(sudoku_grid, .row, .column)
+        sudoku_choices <- update_choices_xy(sudoku_choices, .row, .column, choices)
+      }
+    }
+  }
+  sudoku_choices %>% 
+    arrange(row, column)
+}
+
 firstpass <- TRUE
 continue <- TRUE
-prompt <- FALSE
+prompt <- TRUE
 n_filled <- sum(!is.na(sudoku_grid$value))
 while(any(is.na(sudoku_grid$value)) & continue) {
+  if (firstpass) {
+    sudoku_choices <- update_choices_all(sudoku_choices, sudoku_grid)
+    firstpass <- FALSE
+  }
   for (row in 1:9) {
+    # interrupt if signal is toggled
     if (!continue) break
     for (column in 1:9) {
+      # interrupt if signal is toggled
       if (!continue) break
       if (!is.na(sudoku_grid[sudoku_grid$row == row & sudoku_grid$column == column, "value"])) {
         next
       }
-      choices <- get_cell_choices(sudoku_grid, row, column)
-      message("== choices ==")
-      message("row: ", row, ", column: ", column, ", choice: ", choices)
-      sudoku_choices <- update_choices_xy(sudoku_choices, row, column, choices)
-      if (!firstpass){
-        only_choice <- test_choices_xy(sudoku_grid, sudoku_choices, row, column)
-        message("== only_choice ==")
-        message("row: ", row, ", column: ", column, ", only_choice: ", only_choice)
-        if (length(only_choice) == 1) {
-          if (prompt) {
-            continue <- prompt_next()
-          } else {
-            continue <- TRUE
-          }
-          if (continue) {
-            sudoku_grid <- add_value_xy(sudoku_grid, row, column, only_choice, given = FALSE)
-            print(plot_grid(sudoku_grid))
-            Sys.sleep(0.5)
-          }
+      only_choice <- test_choices_xy(sudoku_grid, sudoku_choices, row, column)
+      message("== only_choice ==")
+      message("row: ", row, ", column: ", column, ", only_choice: ", only_choice)
+      if (length(only_choice) == 1) {
+        if (prompt) {
+          continue <- prompt_next()
+        } else {
+          continue <- TRUE
+        }
+        if (continue) {
+          sudoku_grid <- add_value_xy(sudoku_grid, row, column, only_choice, given = FALSE)
+          print(plot_grid(sudoku_grid))
+          Sys.sleep(0.5)
+          sudoku_choices <- update_choices_all(sudoku_choices, sudoku_grid)
+          next
+        }
+      }
+      cell_choices <- get_cell_choices(sudoku_choices, row, column)
+      only_cell <- vapply(cell_choices, only_cell_in_tile, logical(1), .choices = sudoku_choices, .row = row, .column = column)
+      value <- cell_choices[only_cell]
+      message("== only cell ==")
+      message("row: ", row, ", column: ", column, ", value: ", value)
+      if (sum(only_cell) == 1) {
+        if (prompt) {
+          continue <- prompt_next()
+        } else {
+          continue <- TRUE
+        }
+        if (continue) {
+          sudoku_grid <- add_value_xy(sudoku_grid, row, column, value, given = FALSE)
+          print(plot_grid(sudoku_grid))
+          Sys.sleep(0.5)
+          sudoku_choices <- update_choices_all(sudoku_choices, sudoku_grid)
+          next
         }
       }
     }
   }
-  firstpass <- FALSE
   new_n_filled <- sum(!is.na(sudoku_grid$value))
-  if (identical(new_n_filled, n_filled)) {
+  if (continue & identical(new_n_filled, n_filled)) {
     message("No progress in last round. No point trying again.")
     break
   }
