b9fa99f765f93401a2c9332d37bbae22b382b5a4
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2023-07-17 10:39:06 +0100

    rearrange code; introduced mistake though

--- 02.R
+++ 02.R
@@ -1,5 +1,11 @@
 library(tidyverse)
 
+sudoku_choices <- tibble(
+  value = integer(0),
+  row = integer(0),
+  column = integer(0)
+)
+
 initiate_empty_grid <- function() {
   tibble(
     value = NA,
@@ -9,91 +15,12 @@ initiate_empty_grid <- function() {
     arrange(row, column)
 }
 
-sudoku_choices <- tibble(
-  value = integer(0),
-  row = integer(0),
-  column = integer(0)
-)
-
 add_value_xy <- function(.grid, .row, .column, value, given=FALSE) {
   idx <- which(.grid$row == .row & .grid$column == .column)
   .grid[idx, "value"] <- value
   .grid[idx, "given"] <- given
   .grid
 }
-
-update_choices_xy <- function(.grid, .row, .column, choices) {
-  if (length(choices) == 0) {
-    return(.grid)
-  }
-  .grid %>% 
-    filter(!(row == .row & column == .column)) %>% 
-    bind_rows(tibble(value = choices, row = .row, column = .column)) %>% 
-    arrange(row, column)
-}
-
-plot_grid <- function(.grid) {
-  .grid <- .grid %>% 
-    mutate(given = factor(given, c(TRUE, FALSE)))
-  tile_centers <- expand.grid(row = 2 + 0:2*3, column = 2 + 0:2*3)
-  ggplot(.grid, aes(column, 10-row)) +
-    geom_tile(fill = "white", color = "black", width = 1, height = 1) +
-    geom_tile(aes(10-row, column), tile_centers, fill = NA, color = "black", width = 3, height = 3, linewidth = 2) +
-    geom_text(aes(label = value, color = given)) +
-    theme_void() +
-    scale_color_manual(values = c("TRUE" = "black", "FALSE" = "cornflowerblue"))
-}
-# plot_grid(sudoku_grid)
-
-plot_choices_value <- function(.choices, .value) {
-  tile_centers <- expand.grid(row = 2 + 0:2*3, column = 2 + 0:2*3)
-  .choices %>% 
-    filter(value == .value) %>% 
-  ggplot(aes(column, 10-row)) +
-    geom_tile(fill = "white", color = "black", width = 1, height = 1) +
-    geom_tile(aes(10-row, column), tile_centers, fill = NA, color = "black", width = 3, height = 3, linewidth = 2) +
-    geom_text(aes(label = value)) +
-    theme_void() +
-    scale_color_manual(values = c("TRUE" = "black", "FALSE" = "cornflowerblue"))
-}
-plot_choices_value(sudoku_choices, 1)
-plot_choices_value(sudoku_choices, 4)
-
-plot_choices <- function(.choices) {
-  tile_centers <- expand.grid(row = 2 + 0:2*3, column = 2 + 0:2*3)
-  .choices %>% 
-    group_by(row, column) %>% 
-    summarise(
-      value = strwrap_choices(value),
-      singleton = str_length(value) == 1
-    ) %>%
-    ggplot(aes(column, 10-row)) +
-    geom_tile(fill = "white", color = "black", width = 1, height = 1) +
-    geom_tile(aes(10-row, column), tile_centers, fill = NA, color = "black", width = 3, height = 3, linewidth = 2) +
-    geom_text(aes(label = value, colour = singleton)) +
-    theme_void() +
-    scale_color_manual(values = c("TRUE" = "black", "FALSE" = "cornflowerblue"))
-}
-plot_choices(sudoku_choices)
-
-strwrap_choices <- function(x) {
-  x <- str_flatten(x)
-  lapply(
-    split(
-      str_split(x, "")[[1]],
-      head(rep(1:3, each = 3), str_length(x))
-    ),
-    str_flatten
-  ) %>% str_flatten(collapse = "\n")
-}
-strwrap_choices("123456789")
-strwrap_choices("12345")
-strwrap_choices("12")
-
-sudoku_choices %>% 
-  group_by(row, column) %>% 
-  summarise(value = strwrap_choices(value))
-
 
 sudoku_grid <- initiate_empty_grid()
 sudoku_grid <- add_value_xy(sudoku_grid, 1, 1, 2, given=TRUE)
@@ -120,7 +47,7 @@ sudoku_grid <- add_value_xy(sudoku_grid, 9, 1, 1, given=TRUE)
 sudoku_grid <- add_value_xy(sudoku_grid, 9, 5, 7, given=TRUE)
 sudoku_grid <- add_value_xy(sudoku_grid, 9, 6, 6, given=TRUE)
 sudoku_grid <- add_value_xy(sudoku_grid, 9, 9, 3, given=TRUE)
-plot_grid(sudoku_grid)
+sudoku_grid
 
 get_tile_index <- function(index) {
   (index-1) %/% 3 + 1
@@ -140,7 +67,59 @@ get_tile_values <- function(.grid, .row, .column) {
     pull(value)
 }
 
-compute_cell_choices <- function(.grid, .row, .column) {
+exclude_choice_required_elsewhere <- function(.value, .choices, .row, .column) {
+  # look for row/columns where a number cannot appear in 2 out of 3 tiles (on that row)
+  # deduct that the choice MUST appear on that row/column in the remaining tile
+  # eliminate that choice from the other rows/columns in that remaining tile
+  
+  # considering that this function focuses on a given row/column,
+  # it needs to look at the other two rows for that tile
+  row_tile <- get_tile_index(.row)
+  column_tile <- get_tile_index(.column)
+  tile_rows <- get_tile_indices(row_tile)
+  tile_columns <- get_tile_indices(column_tile)
+  tile_other_rows <- setdiff(tile_rows, .row)
+  tile_other_columns <- setdiff(tile_columns, .column)
+  other_tiles_rows <- setdiff(1:9, tile_rows)
+  other_tiles_columns <- setdiff(1:9, tile_columns)
+  
+  # which other rows in the tile is this value a choice for?
+  choice_other_rows <- .choices %>% 
+    filter(value == .value & row %in% tile_other_rows & column %in% tile_columns) %>% 
+    pull(row) %>% 
+    unique()
+  # exclude the value from this cell if it is required in any other row in that tile
+  for (row in choice_other_rows) {
+    choices_in_other_row <- .choices %>% 
+      filter(row %in% choice_other_rows & !column %in% tile_columns) %>% 
+      pull(value) %>% 
+      unique() %>% 
+      sort()
+    if (!.value %in% choices_in_other_row) {
+      return(TRUE) # exclude
+    }
+  }
+  # which other rows in the tile is this value a choice for?
+  choice_other_columns <- .choices %>% 
+    filter(value == .value & column %in% tile_other_columns & row %in% tile_rows) %>% 
+    pull(column) %>% 
+    unique()
+  # exclude the value from this cell if it is required in any other column in that tile
+  for (column in choice_other_columns) {
+    choices_in_other_column <- .choices %>% 
+      filter(column %in% choice_other_columns & !row %in% tile_rows) %>% 
+      pull(value) %>% 
+      unique() %>% 
+      sort()
+    if (!.value %in% choices_in_other_column) {
+      return(TRUE) # exclude
+    }
+  }
+  return(FALSE)
+}
+exclude_choice_required_elsewhere(8, sudoku_choices, 1, 2)
+
+compute_cell_choices <- function(.grid, .choices, .row, .column) {
   grid_value <- .grid %>% 
     filter(row == .row & column == .column) %>% 
     pull(value)
@@ -163,14 +142,111 @@ compute_cell_choices <- function(.grid, .row, .column) {
   tile_values_used <- get_tile_values(.grid, .row, .column)
   choices <- setdiff(choices, tile_values_used)
   
+  print(choices)
+  exclude <- vapply(choices, exclude_choice_required_elsewhere, FUN.VALUE = logical(1), .choices = .choices, .row = .row, .column = .column)
+  print(exclude)
+  choices <- choices[!exclude]
+  print(choices)
+  
   choices
 }
-compute_cell_choices(sudoku_grid, 1, 3)
-compute_cell_choices(sudoku_grid, 2, 4)
-compute_cell_choices(sudoku_grid, 3, 8)
-compute_cell_choices(sudoku_grid, 6, 8)
-compute_cell_choices(sudoku_grid, 8, 7)
-compute_cell_choices(sudoku_grid, 1, 2)
+compute_cell_choices(sudoku_grid, sudoku_choices, 1, 3)
+compute_cell_choices(sudoku_grid, sudoku_choices, 2, 4)
+compute_cell_choices(sudoku_grid, sudoku_choices, 3, 8)
+compute_cell_choices(sudoku_grid, sudoku_choices, 6, 8)
+compute_cell_choices(sudoku_grid, sudoku_choices, 8, 7)
+compute_cell_choices(sudoku_grid, sudoku_choices, 1, 2)
+
+update_choices_xy <- function(.grid, .row, .column, choices) {
+  if (length(choices) == 0) {
+    return(.grid)
+  }
+  .grid %>% 
+    filter(!(row == .row & column == .column)) %>% 
+    bind_rows(tibble(value = choices, row = .row, column = .column)) %>% 
+    arrange(row, column)
+}
+
+update_choices_all <- function(sudoku_choices, sudoku_grid) {
+  for (.row in 1:9) {
+    for (.column in 1:9) {
+      sudoku_grid_value <- sudoku_grid %>% 
+        filter(row == .row & column == .column) %>% 
+        pull(value)
+      if (!is.na(sudoku_grid_value)) {
+        sudoku_choices <- update_choices_xy(sudoku_choices, .row, .column, sudoku_grid_value)
+      } else {
+        choices <- compute_cell_choices(sudoku_grid, sudoku_choices, .row, .column)
+        sudoku_choices <- update_choices_xy(sudoku_choices, .row, .column, choices)
+      }
+    }
+  }
+  sudoku_choices %>% 
+    arrange(row, column)
+}
+sudoku_choices <- update_choices_all(sudoku_choices, sudoku_grid)
+
+plot_grid <- function(.grid) {
+  .grid <- .grid %>% 
+    mutate(given = factor(given, c(TRUE, FALSE)))
+  tile_centers <- expand.grid(row = 2 + 0:2*3, column = 2 + 0:2*3)
+  ggplot(.grid, aes(column, 10-row)) +
+    geom_tile(fill = "white", color = "black", width = 1, height = 1) +
+    geom_tile(aes(10-row, column), tile_centers, fill = NA, color = "black", width = 3, height = 3, linewidth = 2) +
+    geom_text(aes(label = value, color = given)) +
+    theme_void() +
+    scale_color_manual(values = c("TRUE" = "black", "FALSE" = "cornflowerblue"))
+}
+plot_grid(sudoku_grid)
+
+plot_choices_value <- function(.choices, .value) {
+  tile_centers <- expand.grid(row = 2 + 0:2*3, column = 2 + 0:2*3)
+  .choices %>% 
+    filter(value == .value) %>% 
+    ggplot(aes(column, 10-row)) +
+    geom_tile(fill = "white", color = "black", width = 1, height = 1) +
+    geom_tile(aes(10-row, column), tile_centers, fill = NA, color = "black", width = 3, height = 3, linewidth = 2) +
+    geom_text(aes(label = value)) +
+    theme_void() +
+    scale_color_manual(values = c("TRUE" = "black", "FALSE" = "cornflowerblue"))
+}
+plot_choices_value(sudoku_choices, 1)
+plot_choices_value(sudoku_choices, 4)
+
+strwrap_choices <- function(x) {
+  x <- str_flatten(x)
+  lapply(
+    split(
+      str_split(x, "")[[1]],
+      head(rep(1:3, each = 3), str_length(x))
+    ),
+    str_flatten
+  ) %>% str_flatten(collapse = "\n")
+}
+strwrap_choices("123456789")
+strwrap_choices("12345")
+strwrap_choices("12")
+
+sudoku_choices %>% 
+  group_by(row, column) %>% 
+  summarise(value = strwrap_choices(value))
+
+plot_choices <- function(.choices) {
+  tile_centers <- expand.grid(row = 2 + 0:2*3, column = 2 + 0:2*3)
+  .choices %>% 
+    group_by(row, column) %>% 
+    summarise(
+      value = strwrap_choices(value),
+      singleton = str_length(value) == 1
+    ) %>%
+    ggplot(aes(column, 10-row)) +
+    geom_tile(fill = "white", color = "black", width = 1, height = 1) +
+    geom_tile(aes(10-row, column), tile_centers, fill = NA, color = "black", width = 3, height = 3, linewidth = 2) +
+    geom_text(aes(label = value, colour = singleton)) +
+    theme_void() +
+    scale_color_manual(values = c("TRUE" = "black", "FALSE" = "cornflowerblue"))
+}
+plot_choices(sudoku_choices)
 
 get_cell_choices <- function(.grid, .row, .column) {
   .grid %>% 
@@ -245,24 +321,6 @@ test_choices_xy <- function(.grid, .choices, .row, .column) {
 }
 test_choices_xy(sudoku_grid, sudoku_choices, 3, 8)
 
-update_choices_all <- function(sudoku_choices, sudoku_grid) {
-  for (.row in 1:9) {
-    for (.column in 1:9) {
-      sudoku_grid_value <- sudoku_grid %>% 
-        filter(row == .row & column == .column) %>% 
-        pull(value)
-      if (!is.na(sudoku_grid_value)) {
-        sudoku_choices <- update_choices_xy(sudoku_choices, .row, .column, sudoku_grid_value)
-      } else {
-        choices <- compute_cell_choices(sudoku_grid, .row, .column)
-        sudoku_choices <- update_choices_xy(sudoku_choices, .row, .column, choices)
-      }
-    }
-  }
-  sudoku_choices %>% 
-    arrange(row, column)
-}
-
 firstpass <- TRUE
 continue <- TRUE
 prompt <- F
@@ -297,6 +355,8 @@ while(any(is.na(sudoku_grid$value)) & continue) {
           Sys.sleep(0.5)
           sudoku_choices <- update_choices_all(sudoku_choices, sudoku_grid)
           next
+        } else {
+          break
         }
       }
       # only cell in tile for this value?
@@ -317,6 +377,8 @@ while(any(is.na(sudoku_grid$value)) & continue) {
           Sys.sleep(0.5)
           sudoku_choices <- update_choices_all(sudoku_choices, sudoku_grid)
           next
+        } else {
+          break
         }
       }
       # only cell in column for value?
@@ -336,6 +398,8 @@ while(any(is.na(sudoku_grid$value)) & continue) {
           Sys.sleep(0.5)
           sudoku_choices <- update_choices_all(sudoku_choices, sudoku_grid)
           next
+        } else {
+          break
         }
       }
       # only cell in row for value?
@@ -355,6 +419,8 @@ while(any(is.na(sudoku_grid$value)) & continue) {
           Sys.sleep(0.5)
           sudoku_choices <- update_choices_all(sudoku_choices, sudoku_grid)
           next
+        } else {
+          break
         }
       }
     }
